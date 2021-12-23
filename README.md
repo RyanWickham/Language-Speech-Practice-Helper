5b621400ddfd3ec48cff572e3deab81820081f58
Ryan Wickham <48148988+RyanWickham@users.noreply.github.com>
2021-12-23 22:32:26 +1100

    base logic

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Language-Speech-Pronunciation-Practice-Helper
+
 A test application to help practice speaking pronunciation skills for a forign language.
 
 This application is to let the user choose a language, then the user is to write down a word/words/sentence into the text box.
@@ -6,7 +7,13 @@ The user is then to press the record button and speek the word/words there were 
 The application should then display the spoken words on the screen, displaying what was correct and what was wrong with the users pronunciation.
 
 The information displayed should be:
+
 - Display what the system thought was said by the user.
 - Colour the word/character in green if the pronunciation was correct (enough for the system).
 - Colour the word/character wrong if the system determind the pronunciation to be wrong.
 - The confidence of the system in displaying what was said (as a %)
+
+# Used Resources
+
+The Cantonese to Jyutping data/functions are from: https://github.com/hanleyweng/CantoJpMin.
+Credit to hanleywang
--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
-const SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent =
+  SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
 //HTML Fields
-const userInputToTestAgentsPTag = document.getElementById("textToTestAgentsInput");
-const userSpokenResultPTag = document.getElementById("userSpokenResult");
-const systemConfidenceOutputPTag = document.getElementById("systemConfidenceOutput");
-const recordVoiceButton = document.getElementById("recordVoiceButton");
+var userInputToTestAgentsPTag = document.getElementById(
+  'textToTestAgentsInput'
+);
+var userSpokenResultPTag = document.getElementById('userSpokenResult');
+var systemConfidenceOutputPTag = document.getElementById(
+  'systemConfidenceOutput'
+);
+var recordVoiceButton = document.getElementById('recordVoiceButton');
+var resultTable = document.getElementById('resultTable');
 
 function recordVoiceButtonOnClickListener() {
   recordVoiceButton.disabled = true;
   recordVoiceButton.textContent = 'In Progress';
 
   //Get text from the input field
-  const phrase = userInputToTestAgentsPTag.value;
-  
+  var phrase = userInputToTestAgentsPTag.value;
+  console.log('Phrase: ' + phrase);
+
   // To ensure case consistency while checking with the returned output text
   phrase = phrase.toLowerCase();
-  
+
   userSpokenResultPTag.textContent = '...loading';
   systemConfidenceOutputPTag.textContent = '...%';
 
-  const grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + phrase +';';
-  const recognition = new SpeechRecognition();
-  const speechRecognitionList = new SpeechGrammarList();
+  var grammar = '#JSGF V1.0; grammar phrase; public <phrase> = ' + phrase + ';';
+  var recognition = new SpeechRecognition();
+  var speechRecognitionList = new SpeechGrammarList();
   speechRecognitionList.addFromString(grammar, 1);
   recognition.grammars = speechRecognitionList;
   recognition.lang = 'zh-Yue';
@@ -32,7 +39,7 @@ function recordVoiceButtonOnClickListener() {
 
   recognition.start();
 
-  recognition.onresult = function(event) {
+  recognition.onresult = function (event) {
     // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
     // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
     // It has a getter so it can be accessed like an array
@@ -40,71 +47,112 @@ function recordVoiceButtonOnClickListener() {
     // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
     // These also have getters so they can be accessed like arrays.
     // The second [0] returns the SpeechRecognitionAlternative at position 0.
-    // We then return the transcript property of the SpeechRecognitionAlternative object 
+    // We then return the transcript property of the SpeechRecognitionAlternative object
     const speechResult = event.results[0][0].transcript.toLowerCase();
-    
-    userSpokenResultPTag.textContent = speechResult;
-    systemConfidenceOutputPTag.textContent = 'Confidence: ' + (event.results[0][0].confidence * 100).toFixed(2) + '%';
-    
-    if(speechResult === phrase) {
+
+    //Writing results to screen
+    userSpokenResultPTag.textContent =
+      'What the system think you said: \r\n' +
+      speechResult +
+      '\r\n' +
+      convertCharactersToJyutping(speechResult);
+    systemConfidenceOutputPTag.textContent =
+      'Confidence: ' + (event.results[0][0].confidence * 100).toFixed(2) + '%';
+
+    if (speechResult === phrase) {
       userSpokenResultPTag.style.background = 'lime';
     } else {
       userSpokenResultPTag.style.background = 'red';
     }
 
-    console.log('Confidence: ' + event.results[0][0].confidence);
-  }
+    renderResultTable(event.results[0]);
+  };
 
-  recognition.onspeechend = function() {
+  recognition.onspeechend = function () {
     recognition.stop();
     recordVoiceButton.disabled = false;
     recordVoiceButton.textContent = 'Start new test';
-  }
+  };
 
-  recognition.onerror = function(event) {
+  recognition.onerror = function (event) {
     recordVoiceButton.disabled = false;
     recordVoiceButton.textContent = 'Start new test';
-    userSpokenResultPTag.textContent = 'Error occurred in recognition: ' + event.error;
-  }
-  
-  recognition.onaudiostart = function(event) {
-      //Fired when the user agent has started to capture audio.
-      console.log('SpeechRecognition.onaudiostart');
-  }
-  
-  recognition.onaudioend = function(event) {
-      //Fired when the user agent has finished capturing audio.
-      console.log('SpeechRecognition.onaudioend');
-  }
-  
-  recognition.onend = function(event) {
-      //Fired when the speech recognition service has disconnected.
-      console.log('SpeechRecognition.onend');
-  }
-  
-  recognition.onnomatch = function(event) {
-      //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
-      console.log('SpeechRecognition.onnomatch');
-  }
-  
-  recognition.onsoundstart = function(event) {
-      //Fired when any sound — recognisable speech or not — has been detected.
-      console.log('SpeechRecognition.onsoundstart');
-  }
-  
-  recognition.onsoundend = function(event) {
-      //Fired when any sound — recognisable speech or not — has stopped being detected.
-      console.log('SpeechRecognition.onsoundend');
-  }
-  
+    userSpokenResultPTag.textContent =
+      'Error occurred in recognition: ' + event.error;
+  };
+
+  recognition.onaudiostart = function (event) {
+    //Fired when the user agent has started to capture audio.
+    console.log('SpeechRecognition.onaudiostart');
+  };
+
+  recognition.onaudioend = function (event) {
+    //Fired when the user agent has finished capturing audio.
+    console.log('SpeechRecognition.onaudioend');
+  };
+
+  recognition.onend = function (event) {
+    //Fired when the speech recognition service has disconnected.
+    console.log('SpeechRecognition.onend');
+  };
+
+  recognition.onnomatch = function (event) {
+    //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
+    console.log('SpeechRecognition.onnomatch');
+  };
+
+  recognition.onsoundstart = function (event) {
+    //Fired when any sound — recognisable speech or not — has been detected.
+    console.log('SpeechRecognition.onsoundstart');
+  };
+
+  recognition.onsoundend = function (event) {
+    //Fired when any sound — recognisable speech or not — has stopped being detected.
+    console.log('SpeechRecognition.onsoundend');
+  };
+
   recognition.onspeechstart = function (event) {
-      //Fired when sound that is recognised by the speech recognition service as speech has been detected.
-      console.log('SpeechRecognition.onspeechstart');
-  }
-  recognition.onstart = function(event) {
-      //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
-      console.log('SpeechRecognition.onstart');
-  }
-};
+    //Fired when sound that is recognised by the speech recognition service as speech has been detected.
+    console.log('SpeechRecognition.onspeechstart');
+  };
+  recognition.onstart = function (event) {
+    //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
+    console.log('SpeechRecognition.onstart');
+  };
+}
 
 recordVoiceButton.addEventListener('click', recordVoiceButtonOnClickListener);
+
+function convertCharactersToJyutping(characters) {
+  return CantoJpMin.toJyutping(characters);
+}
+
+function renderResultTable(results) {
+  //Clear Table
+
+  //add headers back into table
+
+  //add result data to table
+  for (let i = 0; i < results.length; i++) {
+    //create new row/cells
+    let row = document.createElement('tr');
+    let cellCharacter = document.createElement('td');
+    let cellJyutping = document.createElement('td');
+    let cellCofidence = document.createElement('td');
+
+    //Fill cells with information
+    cellCharacter.innerHTML = results[i].transcript;
+    cellJyutping.innerHTML = convertCharactersToJyutping(results[i].transcript);
+    cellCofidence.innerHTML = (results[i].confidence * 100).toFixed(2) + '%';
+
+    //add cells to row
+    row.appendChild(cellCharacter);
+    row.appendChild(cellJyutping);
+    row.appendChild(cellCofidence);
+
+    //add row to table
+    resultTable.appendChild(row);
+
+    console.log('ROW', row);
+  }
+}
